74d1c8231757f01795bf90c15dc88d933a3a4ac6
Atul Singh <victimizeit@gmail.com>
2016-12-15 09:56:19 +0530

    Create learn.R

---
+++ learn.R
@@ -0,0 +1,10 @@
+# reading the data
+data <- read.csv("song_data.csv", header = TRUE)
+head(data,4) # fetching first 4 rows only
+str(data) # know more about data
+names(data) # get names of columns
+dim(data) # checking dimensions, rows & columns
+nrow(data)
+ncol(data)
+
+# =====================================================================
