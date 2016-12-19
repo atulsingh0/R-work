37940590533d3d6684d8bb122d6d51a8109fc560
atulsingh0 <victimizeit@gmail.com>
2016-12-19 19:25:52 +0530

    commit control flow

---
+++ Learn/R3_Conditionals_Control_Flow.r
@@ -0,0 +1,36 @@
+
+# Relational Operator
+a <- 5
+b <- 4
+v1 <- "Atul"
+v2 <- "Singh"
+
+a == b  # FALSE
+b != a  # TRUE 
+a >= b  # TRUE
+
+v1 < v2 # TRUE as alphabatically A < S
+
+TRUE < FALSE   # as TRUE is 1 and FALSE is 0
+
+
+# compare vector
+fruit1 <- c("Banana", "Apple")
+fruit2 <- c("Apple", "Banana")
+
+fruit1 > fruit2    # TRUE n FALSE as B>A
+
+blog_views <- c(2,3,6,1,4)
+profile_views <- c(2,1,3,5,3)
+
+blog_views > profile_views
+
+
+# compare matrices
+views <- matrix(c(blog_views, profile_views), nrow=2, byrow =TRUE)
+views
+views >= 4
+views == 3
+
+
+# Logical Operator  & (AND), | (OR), 
